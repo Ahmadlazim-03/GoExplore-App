47bb9a312d056f531fef927a24e9f5d6cfa05282
Ahmad Lazim <ahmadlazim422@gmail.com>
2024-11-10 23:10:55 +0700

    Dashboard

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -5,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('/home/index');
+});
+
+Route::get('/dashboard-menu-admin', function () {
+    return view('/admin/dashboard');
+});
+
+
