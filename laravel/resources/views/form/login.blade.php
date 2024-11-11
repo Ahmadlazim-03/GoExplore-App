4788822e1904ca078a61520db1767e5a9e6a09e9
Safinaarm <Safinaf764@gmail.com>
2024-11-11 09:38:16 +0700

    Tambah Halaman Form

---
+++ laravel/resources/views/form/login.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>RegistrationForm_v1 by Colorlib</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<!-- MATERIAL DESIGN ICONIC FONT -->
+		<link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
+
+		<!-- STYLE CSS -->
+		<link rel="stylesheet" href="css/formcss/style.css">
+	</head>
+
+	<body>
+
+		<div class="wrapper" style="background-image: url('images/bg-registration-form-1.jpg');">
+			<div class="inner">
+				<div class="image-holder">
+					<img src="images/registration-form-1.jpg" alt="">
+				</div>
+				<form action="">
+					<h3>Login Form</h3>
+					<div class="form-wrapper">
+						<input type="text" placeholder="Email Address" class="form-control">
+						<i class="zmdi zmdi-email"></i>
+					</div>
+					<div class="form-wrapper">
+						<input type="password" placeholder="Password" class="form-control">
+						<i class="zmdi zmdi-lock"></i>
+					</div>
+					<button>Login
+						<i class="zmdi zmdi-arrow-right"></i>
+					</button>
+                    <div class="form-wrapper">
+						<p style="margin-left:30%; margin-top:10%;">Sign With Google</p>
+					</div>
+				</form>
+			</div>
+		</div>
+		
+	</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -7,5 +7,10 @@ Route::get('/', function () {
     return view('admin/dashboard');
 });
 
+Route::get('/login', function () {
+    return view('form/login');
+}); 
 
-
+Route::get('/register', function () {
+    return view('form/register');
+});
