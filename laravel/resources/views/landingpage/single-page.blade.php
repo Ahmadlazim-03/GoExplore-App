05d910f041f0502804b41cb84f830cc3780a34de
reynaldisusilowaskito <reeynaldisousiloq2@gmail.com>
2024-12-15 15:26:23 +0700

    saya menambahkan

--- laravel/resources/views/landingpage/single-page.blade.php
+++ laravel/resources/views/landingpage/single-page.blade.php
@@ -600,68 +600,74 @@
       </div>
     </section> <!-- .section -->
 
-    <footer class="ftco-footer ftco-bg-dark ftco-section">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">dirEngine</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4 ml-md-5">
-              <h2 class="ftco-heading-2">Information</h2>
-              <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">About</a></li>
-                <li><a href="#" class="py-2 d-block">Service</a></li>
-                <li><a href="#" class="py-2 d-block">Terms and Conditions</a></li>
-                <li><a href="#" class="py-2 d-block">Become a partner</a></li>
-                <li><a href="#" class="py-2 d-block">Best Price Guarantee</a></li>
-                <li><a href="#" class="py-2 d-block">Privacy and Policy</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Customer Support</h2>
-              <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">FAQ</a></li>
-                <li><a href="#" class="py-2 d-block">Payment Option</a></li>
-                <li><a href="#" class="py-2 d-block">Booking Tips</a></li>
-                <li><a href="#" class="py-2 d-block">How it works</a></li>
-                <li><a href="#" class="py-2 d-block">Contact Us</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-            	<h2 class="ftco-heading-2">Have a Questions?</h2>
-            	<div class="block-23 mb-3">
-	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
-	              </ul>
-	            </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
-
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-          </div>
-        </div>
-      </div>
-    </footer>
+	<footer class="ftco-footer ftco-bg-dark ftco-section">
+		<div class="container">
+		  <div class="row mb-5">
+			<!-- Go Explore Section -->
+			<div class="col-md-3">
+			  <div class="ftco-footer-widget mb-4">
+				<h2 class="ftco-heading-2 text-white">Go Explore</h2>
+				<p class="text-white">Setiap Langkah di Surabaya Menawarkan Cerita Baru. Bergabunglah dengan Kami dalam Menyusuri Kota Ini dan Temukan Keajaiban yang Menanti.</p>
+				<ul class="ftco-footer-social list-unstyled mt-5">
+				  <li class="ftco-animate"><a href="#" class="text-white"><span class="icon-twitter"></span></a></li>
+				  <li class="ftco-animate"><a href="#" class="text-white"><span class="icon-facebook"></span></a></li>
+				  <li class="ftco-animate"><a href="#" class="text-white"><span class="icon-instagram"></span></a></li>
+				</ul>
+			  </div>
+			</div>
+	  
+			<!-- Information Section -->
+			<div class="col-md-3">
+			  <div class="ftco-footer-widget mb-4">
+				<h2 class="ftco-heading-2 text-white">Information</h2>
+				<ul class="list-unstyled">
+				  <li><a href="#" class="py-2 d-block text-white">About</a></li>
+				  <li><a href="#" class="py-2 d-block text-white">Destination</a></li>
+				  <li><a href="#" class="py-2 d-block text-white">Login</a></li>
+				  <li><a href="#" class="py-2 d-block text-white">Become a partner</a></li>
+				  <li><a href="#" class="py-2 d-block text-white">Best Price Guarantee</a></li>
+				  <li><a href="#" class="py-2 d-block text-white">Privacy and Policy</a></li>
+				</ul>
+			  </div>
+			</div>
+	  
+			<!-- Customer Support Section -->
+			<div class="col-md-3">
+			  <div class="ftco-footer-widget mb-4">
+				<h2 class="ftco-heading-2 text-white">Customer Support</h2>
+				<ul class="list-unstyled">
+				  <li><a href="#" class="py-2 d-block text-white">FAQ</a></li>
+				  <li><a href="#" class="py-2 d-block text-white">Payment Option</a></li>
+				  <li><a href="#" class="py-2 d-block text-white">Booking Tips</a></li>
+				  <li><a href="#" class="py-2 d-block text-white">How it works</a></li>
+				  <li><a href="#" class="py-2 d-block text-white">Contact Us</a></li>
+				</ul>
+			  </div>
+			</div>
+	  
+			<!-- Contact Information Section -->
+			<div class="col-md-3">
+			  <div class="ftco-footer-widget mb-4">
+				<h2 class="ftco-heading-2 text-white">Have a Questions?</h2>
+				<div class="block-23 mb-3">
+				  <ul>
+					<li><span class="icon icon-map-marker text-white"></span><span class="text text-white">Surabaya</span></li>
+					<li><a href="#" class="text-white"><span class="icon icon-phone"></span><span class="text text-white">+62 12345678</span></a></li>
+					<li><a href="#" class="text-white"><span class="icon icon-envelope"></span><span class="text text-white">@goexplorecom</span></a></li>
+				  </ul>
+				</div>
+			  </div>
+			</div>
+		  </div>
+	  
+		  <!-- Footer Bottom Section -->
+		  <div class="row">
+			<div class="col-md-12 text-center">
+			  <p class="text-white">© 2024 Go Explore. All Rights Reserved.</p>
+			</div>
+		  </div>
+		</div>
+	  </footer>
 
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.js"></script>
