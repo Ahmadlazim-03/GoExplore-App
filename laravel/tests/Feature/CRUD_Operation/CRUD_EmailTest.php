8cf2c6dd3ad1a32401e270ebb1c0cd345487b31e
Ahmad Lazim <ahmadlazim422@gmail.com>
2025-03-02 01:40:39 +0700

    Update TestCase

--- laravel/tests/Feature/CRUD_Operation/CRUD_EmailTest.php
+++ laravel/tests/Feature/CRUD_Operation/CRUD_EmailTest.php
@@ -2,24 +2,61 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Email;
 use PHPUnit\Framework\Attributes\Test;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CRUD_EmailTest extends TestCase
 {
     use RefreshDatabase;
 
     #[Test]
-    public function user_can_read_email()
-    {
-        
-    }
-    #[Test]
     public function user_can_create_email()
     {
-        
+        $user = User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('password123'),
+            'role' => 2, 
+        ]);
+        $this->actingAs($user);
+        $data_email = [
+            'name' => "user",
+            'email' => "user@gmail.com",
+            'subject' => "ini adalah subjek",
+            'pesan' => "ini adalah pesan"
+        ];
+        $this->post('/send-email', $data_email);
+        $this->assertDatabaseHas('emails',[
+            'name' => "user",
+            'email' => "user@gmail.com",
+            'subject' => "ini adalah subjek",
+            'pesan' => "ini adalah pesan"
+        ]);
     }
-
+    #[Test]
+    public function admin_can_read_email()
+    {
+        $admin = User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('password123'),
+            'role' => 1, 
+        ]);
+        $this->actingAs($admin);
+        $create_email = Email::create([
+            'name' => "user",
+            'email' => "user@gmail.com",
+            'subject' => "ini adalah subjek",
+            'pesan' => "ini adalah pesan"
+        ]);
+        $response = $this->get('/show-email/' . $create_email->id);
+        $response->assertSee('user')
+                 ->assertSee('user@gmail.com')
+                 ->assertSee('ini adalah subjek')
+                 ->assertSee('ini adalah pesan');
+    }
 }
 
--- laravel/tests/Feature/CRUD_Operation/CRUD_RoleTest.php
+++ laravel/tests/Feature/CRUD_Operation/CRUD_RoleTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Role;
+use App\Models\User;
 use PHPUnit\Framework\Attributes\Test;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CRUD_RoleTest extends TestCase
 {
@@ -14,7 +16,18 @@ class CRUD_RoleTest extends TestCase
     #[Test]
     public function admin_can_read_role()
     {
-       
+        $admin = User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('password123'),
+            'role' => 1, 
+        ]);
+        $this->actingAs($admin);
+        Role::create([
+            'name' => "user"
+        ]);
+        $response = $this->get('manajemenrole');
+        $response->assertSee('user');
     }
 
     #[Test]
